89102f00d116d81c86e2b2e8373c0744d500393a
ayoubtahir <ayoub.tahir.2021@gmail.com>
2024-03-28 11:26:33 +0000

    add google provider

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,34 @@
 import { Inter } from "next/font/google";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <></>;
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <main className="bg-slate-100">
+        <div className="h-screen w-full flex justify-center items-center">
+          <button
+            onClick={() => signIn("google")}
+            className="bg-blue-600 text-white text-[18px] font-medium p-4 rounded"
+          >
+            Sign In With Google
+          </button>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main className="bg-slate-100">
+      <div className="h-screen w-full flex justify-center items-center">
+        <button
+          onClick={() => signOut()}
+          className="bg-blue-600 text-white text-[18px] font-medium p-4 rounded"
+        >
+          Log Out as {session.user.email}
+        </button>
+      </div>
+    </main>
+  );
 }
